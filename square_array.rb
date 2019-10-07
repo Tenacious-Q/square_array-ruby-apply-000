0225dc68513bb7d86695a95cd629cdc0b000cd9f
dazzling-node-4320 <mikequigley@quigleypublishing.com>
2019-10-07 15:02:10 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,8 +1,10 @@
 
 
 def square_array (numbers)
-numbers.each do |number|
-  puts "The square of #{number} is #{number*number}."
-
-end
+  new_array = []
+  numbers.each do |number|
+    #puts "The square of #{number} is #{number*number}."
+    new_array << number * number
+  end
+  new_array
 end
